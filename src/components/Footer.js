8eca618540cd8a0306fa2a580627edae69f55596
Ashik S Nair <aashiq5342@gmail.com>
2021-11-24 11:31:07 +0530

    copying to clipboard done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import Mail from "../images/Mail.png";
 import LinkedIn from "../images/LinkedIn.png";
 import GitHub from "../images/GitHub.png";
 import Dribbble from "../images/Dribbble.png";
-import Arrow from '../images/Arrow 1.svg';
+import Arrow from "../images/Arrow 1.svg";
 
 function Footer() {
+
+  const [activate, setactivate] = useState(false);
+
+  const CopyToClipboard = () => {
+    let elementText = "aashiq5342@gmail.com";
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('value', elementText);
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.parentNode.removeChild(inputElement);
+
+    setactivate(true)
+    setTimeout(() => {
+      setactivate(false);
+    },5000);
+
+  };
+
   return (
     <section className="footer-section" id="footer">
       <div className="wrap footer-wrap">
@@ -18,9 +37,14 @@ function Footer() {
           </h1>
           <div className="footer-cta-section">
             <button className="footer-container-LT-btn cta-interative cta-LT">
-              <p>LET'S TALK</p><img src={Arrow} alt="" />
+              <p>LET'S TALK</p>
+              <img src={Arrow} alt="" />
             </button>
-            <button className="footer-container-email-btn cta-EMAIL">
+            <button
+        
+              className={`footer-container-email-btn cta-EMAIL ${activate ? "activate" : " "}`}
+              onClick={CopyToClipboard}
+            >
               aashiq5342@gmail.com
             </button>
           </div>
@@ -28,16 +52,20 @@ function Footer() {
         <div className="footer-bottom">
           <div className="wrap footer-bottom-wrap">
             <div className="footer-bottom-social-icons">
-              <a href="/" className="footer-bottom-icon" content='Email' >
+              <a href="/" className="footer-bottom-icon" content="Email">
                 <img src={Mail} alt="" className="footer-bottom-icon-img" />
               </a>
-              <a href="/" className="footer-bottom-icon" content='LinkedIn' >
-                <img src={LinkedIn} alt="" className="footer-bottom-icon-img" />
+              <a href="/" className="footer-bottom-icon" content="LinkedIn">
+                <img
+                  src={LinkedIn}
+                  alt=""
+                  className="footer-bottom-icoan-img"
+                />
               </a>
-              <a href="/" className="footer-bottom-icon" content='GitHub' >
+              <a href="/" className="footer-bottom-icon" content="GitHub">
                 <img src={GitHub} alt="" className="footer-bottom-icon-img" />
               </a>
-              <a href="/" className="footer-bottom-icon" content='Dribbble' >
+              <a href="/" className="footer-bottom-icon" content="Dribbble">
                 <img src={Dribbble} alt="" className="footer-bottom-icon-img" />
               </a>
             </div>
