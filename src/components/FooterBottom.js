54bcc8886a0008b4c53484bb391128029cf8bba7
Ashik S Nair <aashiq5342@gmail.com>
2021-11-30 13:14:12 +0530

    social links added

--- src/components/FooterBottom.js
+++ src/components/FooterBottom.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Mail from "../images/Mail.png";
 import LinkedIn from "../images/LinkedIn.png";
 import GitHub from "../images/GitHub.png";
@@ -9,16 +10,16 @@ export default function FooterBottom() {
     <div className="footer-bottom">
       <div className="wrap footer-bottom-wrap">
         <div className="footer-bottom-social-icons">
-          <a href="/" className="footer-bottom-icon" content="Email">
+          <Link to="/contact" className="footer-bottom-icon" content="Email">
             <img src={Mail} alt="" className="footer-bottom-icon-img" />
-          </a>
-          <a href="/" className="footer-bottom-icon" content="LinkedIn">
+          </Link>
+          <a href="https://www.linkedin.com/in/ashiksudhakaran/" className="footer-bottom-icon" content="LinkedIn">
             <img src={LinkedIn} alt="" className="footer-bottom-icon-img" />
           </a>
-          <a href="/" className="footer-bottom-icon" content="GitHub">
+          <a href="https://github.com/ASHIK-S-NAIR" className="footer-bottom-icon" content="GitHub">
             <img src={GitHub} alt="" className="footer-bottom-icon-img" />
           </a>
-          <a href="/" className="footer-bottom-icon" content="Dribbble">
+          <a href="https://dribbble.com/Ashik_S_Nair" className="footer-bottom-icon" content="Dribbble">
             <img src={Dribbble} alt="" className="footer-bottom-icon-img" />
           </a>
         </div>
